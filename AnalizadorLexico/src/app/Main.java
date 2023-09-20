2034aa5decc5ef6a5c17bd1baa6d78429b307293
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-09-20 11:15:38 -0300

    update: actualiza clase Main y Lexico.flex

--- AnalizadorLexico/src/app/Main.java
+++ AnalizadorLexico/src/app/Main.java
@@ -1,10 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package app;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        try {
+            // TODO code application logic here
+            FileReader f = new FileReader("C:/prueba.txt");
+            Lexico Lexer = new Lexico(f);
+            Lexer.next_token();
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se encontró el archivo");
+        }
+        
+    }
+    
 }
--- Lexico.flex
+++ Lexico.flex
@@ -1,4 +1,4 @@
-package ejemploflex;
+package app;
 import java_cup.runtime.*;
 import java.util.*;
 
